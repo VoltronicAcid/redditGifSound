569304bb29742dd8d8893f7cd3ae7d8e659bf27e
savedev <savedev@example.com>
2021-11-06 16:05:21 -0400

    initial commit

---
+++ redditGifSound.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Reddit Gif Sound
+// @namespace    https://github.com/VoltronicAcid/
+// @downloadURL  https://github.com/VoltronicAcid/redditGifSound/raw/main/vidsOnly.user.js
+// @version      0.0.1
+// @description  Remember 'mute' setting for gifs on reddit
+// @author       VoltronicAcid
+// @match        https://www.redgifs.com/ifr/*
+// @match        https://www.redgifs.com/watch/*
+// ==/UserScript==
+
+(() => {
+    console.log("RedditGifSound");
+})();
