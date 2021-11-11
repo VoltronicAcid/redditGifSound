434e5d0cc1adcd42ecd9188ff120a3001f704945
savedev <savedev@example.com>
2021-11-11 18:22:47 -0500

    get selected node

--- redditGifSound.user.js
+++ redditGifSound.user.js
@@ -7,10 +7,21 @@
 // @author       VoltronicAcid
 // @match        https://www.redgifs.com/ifr/*
 // @match        https://www.redgifs.com/watch/*
+// @match        https://*reddit.com/*
 // ==/UserScript==
 
-console.log("RedditGifSound 0.0.1\nOutside IIFE");
-
 (() => {
-    console.log("Inside IIFE");
+    let redgif = evt => {
+        if (evt.key === 'j' || evt.key === 'k'){
+            console.log(evt);
+        }
+    };
+
+    document.addEventListener('keydown', evt => {
+        let curr;
+        if (evt.key === 'j' || evt.key === 'k') {
+            curr = document.querySelector('div.res-selected.RES-keyNav-activeElement > div.res-expando-box');
+            console.log(curr);
+        }
+    });
 })();
