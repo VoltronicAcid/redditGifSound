0ad87852b952b013c99a5370aaa3e267cf203e23
savedev <savedev@example.com>
2021-11-12 11:17:09 -0500

    redgif entries collapses/expanded

--- redditGifSound.user.js
+++ redditGifSound.user.js
@@ -6,22 +6,45 @@
 // @description  Remember 'mute' setting for gifs on reddit
 // @author       VoltronicAcid
 // @match        https://www.redgifs.com/ifr/*
+// @match        https://redgifs.com/ifr/*
 // @match        https://www.redgifs.com/watch/*
 // @match        https://*reddit.com/*
 // ==/UserScript==
 
 (() => {
-    let redgif = evt => {
-        if (evt.key === 'j' || evt.key === 'k'){
-            console.log(evt);
+    let redgif = curr => {
+        const inner = curr.querySelector('div.res-expando-box-inner > div.res-iframe-expando');
+
+        if (inner) {
+            console.log('Expanded');
+        } else {
+            console.log('Collapsed');
         }
     };
 
     document.addEventListener('keydown', evt => {
         let curr;
-        if (evt.key === 'j' || evt.key === 'k') {
+        if (evt.key === 'j' || evt.key === 'k' || evt.key === 'x') {
             curr = document.querySelector('div.res-selected.RES-keyNav-activeElement > div.res-expando-box');
-            console.log(curr);
+            console.log(evt.key, 'pressed');
+            console.log(evt);
+
+            if (curr) {
+                const host = curr.getAttribute('data-host');        // redgifs vreddit imgur
+                console.log(host);
+
+                if (host === 'redgifs') {
+                    redgif(curr);
+                }
+
+                if (host === 'vreddit') {
+                    console.log('Reddit video host');
+                }
+
+                if (host === 'imgur') {
+                    console.log('Imgur host');
+                }
+            }
         }
     });
 })();
