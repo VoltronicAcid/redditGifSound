f96178701d400136e4bf0591e235b6a329e9fc3d
savedev <savedev@example.com>
2021-11-14 17:09:37 -0500

    use redgifs settings

--- redditGifSound.user.js
+++ redditGifSound.user.js
@@ -3,48 +3,28 @@
 // @namespace    https://github.com/VoltronicAcid/
 // @downloadURL  https://raw.githubusercontent.com/VoltronicAcid/redditGifSound/master/redditGifSound.user.js
 // @version      0.0.1
-// @description  Remember 'mute' setting for gifs on reddit
+// @description  Remember mute setting for gifs on reddit
 // @author       VoltronicAcid
+// @run-at       document-idle
 // @match        https://www.redgifs.com/ifr/*
-// @match        https://redgifs.com/ifr/*
-// @match        https://www.redgifs.com/watch/*
-// @match        https://*reddit.com/*
 // ==/UserScript==
 
 (() => {
-    let redgif = curr => {
-        const inner = curr.querySelector('div.res-expando-box-inner > div.res-iframe-expando');
+    const unMuteGifs = localStorage.getItem('user_sound')
+    
+    const intrvlId = setInterval(() => {
+        const buttonsContainer = document.querySelector('div.options-buttons')
 
-        if (inner) {
-            console.log('Expanded');
-        } else {
-            console.log('Collapsed');
-        }
-    };
-
-    document.addEventListener('keydown', evt => {
-        let curr;
-        if (evt.key === 'j' || evt.key === 'k' || evt.key === 'x') {
-            curr = document.querySelector('div.res-selected.RES-keyNav-activeElement > div.res-expando-box');
-            console.log(evt.key, 'pressed');
-            console.log(evt);
-
-            if (curr) {
-                const host = curr.getAttribute('data-host');        // redgifs vreddit imgur
-                console.log(host);
-
-                if (host === 'redgifs') {
-                    redgif(curr);
-                }
-
-                if (host === 'vreddit') {
-                    console.log('Reddit video host');
-                }
-
-                if (host === 'imgur') {
-                    console.log('Imgur host');
-                }
+        if (buttonsContainer) {
+            if (unMuteGifs === 'unmuted') {
+                const unmuteBttn = buttonsContainer.querySelector('span.icon.icon-mute')
+                unmuteBttn && unmuteBttn.click()
+            } else if (unMuteGifs === 'muted') {
+                const muteButton = buttonsContainer.querySelector('span.icon.icon-sound')
+                muteButton && muteButton.click()
             }
+
+            clearInterval(intrvlId)
         }
-    });
+    }, 100)    
 })();
