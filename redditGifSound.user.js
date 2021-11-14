f8f6238d74390486df4d6f797a79d2ee4df02a41
savedev <savedev@example.com>
2021-11-14 17:20:23 -0500

    remove else

--- redditGifSound.user.js
+++ redditGifSound.user.js
@@ -2,7 +2,7 @@
 // @name         Reddit Gif Sound
 // @namespace    https://github.com/VoltronicAcid/
 // @downloadURL  https://raw.githubusercontent.com/VoltronicAcid/redditGifSound/master/redditGifSound.user.js
-// @version      0.0.1
+// @version      0.0.2
 // @description  Remember mute setting for gifs on reddit
 // @author       VoltronicAcid
 // @run-at       document-idle
@@ -10,18 +10,15 @@
 // ==/UserScript==
 
 (() => {
-    const unMuteGifs = localStorage.getItem('user_sound')
+    const unMuteGifs = localStorage.getItem('user_sound') === 'unmuted'
     
     const intrvlId = setInterval(() => {
         const buttonsContainer = document.querySelector('div.options-buttons')
 
         if (buttonsContainer) {
-            if (unMuteGifs === 'unmuted') {
+            if (unMuteGifs) {
                 const unmuteBttn = buttonsContainer.querySelector('span.icon.icon-mute')
                 unmuteBttn && unmuteBttn.click()
-            } else if (unMuteGifs === 'muted') {
-                const muteButton = buttonsContainer.querySelector('span.icon.icon-sound')
-                muteButton && muteButton.click()
             }
 
             clearInterval(intrvlId)
